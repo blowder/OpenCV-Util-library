4e07434e3a035ecc4ea1890c0276c2c87d7c67b1
Sesshoumaru <blowder@mail.ru>
2015-12-18 01:44:11 +0200

    refactoring

---
+++ src/main/java/ImageProcessingService.java
@@ -0,0 +1,106 @@
+import org.opencv.core.*;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ * Created by sesshoumaru on 17.12.15.
+ */
+public class ImageProcessingService {
+
+    public File detectAndCorrectSkew(File source, File target) {
+        int lowerBorder = -45;
+        int upperBorder = 45;
+
+        Mat temp = OpenCvUtils.erode(Highgui.imread(source.getAbsolutePath()));
+        temp = OpenCvUtils.adaptiveThreshold(temp);
+        Mat detectedLines = OpenCvUtils.getHoughLines(temp);
+        Map<Integer, Integer> angles = OpenCvUtils.calculateAnglesQuantity(detectedLines);
+
+        Map<Integer, Integer> filteredAngles = filterAngles(lowerBorder, upperBorder, angles);
+        int skewAngle = getMostPopularAngle(filteredAngles);
+
+        OpenCvUtils.rotate(Highgui.imread(source.getAbsolutePath()), temp, skewAngle);
+        Highgui.imwrite(target.getAbsolutePath(), temp);
+
+        return target;
+    }
+
+    private Map<Integer, Integer> filterAngles(int lowerBorder, int upperBorder, Map<Integer, Integer> angles) {
+        Map<Integer, Integer> result = new HashMap<Integer, Integer>();
+        for (Map.Entry<Integer, Integer> entry : angles.entrySet())
+            if (entry.getKey() > lowerBorder || entry.getKey() < upperBorder)
+                result.put(entry.getKey(), entry.getValue());
+        return result;
+    }
+
+    private int getMostPopularAngle(Map<Integer, Integer> angles) {
+        List<Map.Entry<Integer, Integer>> result = new ArrayList<Map.Entry<Integer, Integer>>(angles.entrySet());
+        result.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return Integer.compare(o2.getValue(), o1.getValue());
+            }
+        });
+        return result.get(0).getKey();
+    }
+
+
+    public File prepareForSend(File source, File target) {
+        Size blurSize = new Size(2, 2);
+        int minRectangleArea = 30 * 30;
+        int outline = 20;
+
+        Mat temp = Highgui.imread(source.getAbsolutePath());
+        temp = OpenCvUtils.adaptiveThreshold(temp);
+        Imgproc.blur(temp, temp, blurSize);
+
+        Highgui.imwrite(target.getAbsolutePath(), temp);
+
+        List<Rect> rectangles = OpenCvUtils.detectLetters(temp);
+        if (rectangles.size() == 0)
+            throw new RuntimeException("prepareForSend(): Text not found on " + source);
+
+        Iterator<Rect> iterator = rectangles.iterator();
+        while (iterator.hasNext())
+            if (iterator.next().area() < minRectangleArea)
+                iterator.remove();
+
+        List<Point> points = new ArrayList<Point>();
+        for (Rect rect : rectangles) {
+            Core.rectangle(temp, rect.tl(), rect.br(), new Scalar(0, 0, 0));
+            points.add(rect.br());
+            points.add(rect.tl());
+        }
+
+        Point[] points2 = points.toArray(new Point[points.size()]);
+
+        MatOfPoint matOfPoint = new MatOfPoint();
+        matOfPoint.fromArray(points2);
+        Rect bb = Imgproc.boundingRect(matOfPoint);
+
+        bb = addOutline(bb, outline, temp.size());
+
+        //Core.rectangle(temp, resultBB.tl(), resultBB.br(), new Scalar(0, 0, 0));
+
+        Highgui.imwrite(target.getAbsolutePath(), temp.submat(bb));
+        return target;
+    }
+
+    private Rect addOutline(Rect bb, int outline, Size maxBBSize) {
+        Point topLeft = bb.tl();
+        Point bottomRight = bb.br();
+        double newTLX = (topLeft.x - outline) < 0 ? 0 : topLeft.x - outline;
+        double newTLY = (topLeft.y - outline) < 0 ? 0 : topLeft.y - outline;
+        double newBRX = (bottomRight.x + outline) > maxBBSize.width ? maxBBSize.width : bottomRight.x + outline;
+        double newBRY = (bottomRight.y + outline) > maxBBSize.height ? maxBBSize.height : bottomRight.y + outline;
+        return new Rect(new Point(newTLX, newTLY), new Point(newBRX, newBRY));
+    }
+
+    public boolean isBill(File source) {
+
+        return false;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,13 +16,13 @@ public class Main {
     }
 
     public static void main(String... args) throws IOException {
-        File source = new File("C:\\bills\\bilet.JPG");
-        File target = new File("C:\\bills\\target.png");
+        File source = new File("/home/sesshoumaru/bilet.jpg");
+        File target = new File("/home/sesshoumaru/target.png");
 
         long startTime = System.currentTimeMillis();
 
 
-        Mat temp = OpenCvUtils.erode(Highgui.imread(source.getAbsolutePath()));
+        /*Mat temp = OpenCvUtils.erode(Highgui.imread(source.getAbsolutePath()));
         Highgui.imwrite(target.getAbsolutePath(), temp);
 
         temp = OpenCvUtils.adaptiveThreshold(temp);
@@ -46,24 +46,30 @@ public class Main {
         });
 
         OpenCvUtils.rotate(Highgui.imread(source.getAbsolutePath()), temp, sortedAngles.get(sortedAngles.size() - 1).getKey());
-        //Highgui.imwrite(target.getAbsolutePath(), temp);
+        Highgui.imwrite(target.getAbsolutePath(), temp);*/
+        new ImageProcessingService().detectAndCorrectSkew(source,target);
+        new ImageProcessingService().prepareForSend(target,target);
+       /* Mat temp = Highgui.imread(target.getAbsolutePath());
+
         temp = OpenCvUtils.adaptiveThreshold(temp);
-        //OpenCvUtils.adaptiveThreshold(target, target);
-        //temp = Highgui.imread(target.getAbsolutePath());
+        Highgui.imwrite(target.getAbsolutePath(), temp);
+
         Imgproc.blur(temp, temp, new Size(2, 2));
         List<Rect> rectangles = OpenCvUtils.detectLetters(temp);
 
         List<Rect> filteredRectangles = new ArrayList<Rect>();
+        int minRectangleArea = 0 * 15;
         for (Rect rect : rectangles) {
-            if (rect.area() > 20 * 20)
+            if (rect.area() > minRectangleArea)
                 filteredRectangles.add(rect);
         }
         List<Point> points = new ArrayList<Point>();
         for (Rect rect : filteredRectangles) {
-            //Core.rectangle(temp, rect.tl(), rect.br(), new Scalar(0, 0, 0));
+            Core.rectangle(temp, rect.tl(), rect.br(), new Scalar(0, 0, 0));
             points.add(rect.br());
             points.add(rect.tl());
         }
+        Highgui.imwrite(target.getAbsolutePath(), temp);
         MatOfPoint matOfPoint = new MatOfPoint();
         Point[] points2 = points.toArray(new Point[points.size()]);
         matOfPoint.fromArray(points2);
@@ -72,7 +78,7 @@ public class Main {
         Rect resultBB = new Rect(new Point(bb.tl().x - outline, bb.tl().y - outline), new Point(bb.br().x + outline, bb.br().y + outline));
         //Core.rectangle(temp, resultBB.tl(), resultBB.br(), new Scalar(0, 0, 0));
 
-        Highgui.imwrite(target.getAbsolutePath(), temp.submat(resultBB));
+        Highgui.imwrite(target.getAbsolutePath(), temp.submat(resultBB));*/
         System.out.println("Time is: " + (System.currentTimeMillis() - startTime));
 
     }
